46b97d46eed8e2abd679ab5bd13925c76c8580a9
kadrym <km799862@gmail.com>
2025-03-18 12:59:21 +0200

    yml_final

--- test_hospital.py
+++ test_hospital.py
@@ -4,19 +4,19 @@ from hospital import *
 
 
 def test_doctor():
-    doctor= Doctor("kadry","80","dentist")
-    assert doctor.get_details()==["kadry","80","dentist"]
+   doctor= Doctor("kadry","80","dentist")
+   assert doctor.get_details()==["kadry","80","dentist"]
 
 def test_doctor_datatype ():
-    with pytest.raises(TypeError,match="this is wrong please enter the right format"):
-        Doctor(111,12,1)
+   with pytest.raises(TypeError,match="this is wrong please enter the right format"):
+      Doctor(111,12,1)
 
 def test_patient():
     patient=Patient("kadry","80",35,"illness")
     assert patient.get_details_of_patient()==["kadry","80",35,"illness"]
 
 def test_patient_datatype ():
-    with pytest.raises(TypeError,match="this is wrong please enter the right format"):
-        Patient(111,80,50,10)
+   with pytest.raises(TypeError,match="this is wrong please enter the right format"):
+       Patient(111,80,50,10)
 
 
